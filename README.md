15256ac49cf2627989070843eca73cfad98ecad5
Stefan Teunissen <stefanteunissen1@gmail.com>
2023-02-05 20:43:05 +0100

    Compiling gschema in build

--- README.md
+++ README.md
@@ -1,14 +1,10 @@
-# chatgpt-gui
+# ChatGPT GUI
 
 ![cover image](https://github.com/teunissenstefan/chatgpt-gui/blob/master/cover.png?raw=true)
-
 
 ## Building
 ```shell
 git clone git@github.com:teunissenstefan/chatgpt-gui.git
 cd chatgpt-gui
 cargo build --release
-mkdir -p $HOME/.local/share/glib-2.0/schemas
-cp src/org.teunissenstefan.ChatGPT.gschema.xml $HOME/.local/share/glib-2.0/schemas/
-glib-compile-schemas $HOME/.local/share/glib-2.0/schemas/
 ```
--- build.rs
+++ build.rs
@@ -1,7 +1,24 @@
+use std::process::Command;
+use std::env;
+
 fn main() {
     glib_build_tools::compile_resources(
         "src/resources",
         "src/resources/resources.gresource.xml",
         "chatgpt.gresource",
     );
+
+    let home_dir = env::var("HOME").unwrap();
+    let mut schemas_dir = home_dir;
+    schemas_dir.push_str("/.local/share/glib-2.0/schemas");
+    Command::new("mkdir").args(&["-p", &schemas_dir])
+        .status().unwrap();
+    schemas_dir.push_str("/");
+    Command::new("cp").args(&["src/org.teunissenstefan.ChatGPT.gschema.xml", &schemas_dir])
+        .status().unwrap();
+    Command::new("glib-compile-schemas").args(&[&schemas_dir])
+        .status().unwrap();
+
+    println!("cargo:rerun-if-changed=src/resources/resources.gresource.xml");
+    println!("cargo:rerun-if-changed=src/org.teunissenstefan.ChatGPT.gschema.xml");
 }
